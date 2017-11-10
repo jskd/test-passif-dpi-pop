b28a9622e626ad5664f85a84d257def368f2dd45
CactusTribe <lefrancjoaquim@gmail.com>
2017-11-10 14:29:55 +0100

    ajout du test python

---
+++ test.py
@@ -0,0 +1,13 @@
+import dpkt
+
+with open("pop3.pcap", "r", encoding = "ISO-8859-1") as f:
+	pcap = dpkt.pcap.Reader(f)
+
+	for ts, buf in pcap:
+		eth = dpkt.ethernet.Ethernet(buf)
+		ip = eth.data
+		tcp = ip.data
+
+	if tcp.dport == 80 and len(tcp.data) > 0:
+		http = dpkt.http.Request(tcp.data)
+		print(http.uri)
