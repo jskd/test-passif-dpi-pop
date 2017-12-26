c8217fbc08d4c03b03a5117d218869e46b8e8f6e
jskd <git@jeromeskoda.fr>
2017-12-26 17:52:15 +0100

    Clean print

--- test2.py
+++ test2.py
@@ -4,11 +4,48 @@
 
 import dpkt
 import struct
+import datetime
+
+
+import dpkt
+import datetime
+import socket
+from dpkt.compat import compat_ord
+
+
+# https://dpkt.readthedocs.io/en/latest/_modules/examples/print_icmp.html#mac_addr
+def mac_addr(address):
+    """Convert a MAC address to a readable/printable string
+
+       Args:
+           address (str): a MAC address in hex form (e.g. '\x01\x02\x03\x04\x05\x06')
+       Returns:
+           str: Printable/readable MAC address
+    """
+    return ':'.join('%02x' % compat_ord(b) for b in address)
+
+
+def inet_to_str(inet):
+    """Convert inet object to a string
+
+        Args:
+            inet (inet struct): inet network address
+        Returns:
+            str: Printable/readable IP address
+    """
+    # First try ipv4 and then ipv6
+    try:
+        return socket.inet_ntop(socket.AF_INET, inet)
+    except ValueError:
+        return socket.inet_ntop(socket.AF_INET6, inet)
+
+
+
 
 with open("pop3.pcap", "rb") as f:
 	pcap = dpkt.pcap.Reader(f)
 
-	for ts, buf in pcap:
+	for timestamp, buf in pcap:
 
 		# Unpack the Ethernet frame (mac src/dst, ethertype)
 		eth = dpkt.ethernet.Ethernet(buf)
@@ -27,7 +64,21 @@ with open("pop3.pcap", "rb") as f:
 			eth = dpkt.ethernet.Ethernet(buf)
 			tcp = ip.data
 
+			# Pull out fragment information (flags and offset all packed into off field, so use bitmasks)
+			do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
+			more_fragments = bool(ip.off & dpkt.ip.IP_MF)
+			fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
+
 			if  len(tcp.data) > 0:
-				print(tcp.data)
-				print("\n")
+				print ('Data:           %s' % \
+					(str(tcp.data)))
+				print ('Timestamp:      %s' % \
+					(str(datetime.datetime.utcfromtimestamp(timestamp))))
+				print ('Ethernet Frame: %s -> %s (%s)' % \
+					(mac_addr(eth.src), mac_addr(eth.dst), eth.type))
+				print ('IP:             %s -> %s (len=%d ttl=%d DF=%d MF=%d offset=%d)' % \
+					(inet_to_str(ip.src), inet_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset))
+
+				print()
+
 	f.close()
