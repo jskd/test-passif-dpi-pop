58c13870ebf9053e3c2aedb0c31eba2a62ab882a
jskd <git@jeromeskoda.fr>
2017-12-26 17:35:00 +0100

    Add example link

--- test2.py
+++ test2.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+# Inspiration : https://dpkt.readthedocs.io/en/latest/print_http_requests.html
+
 import dpkt
 import struct
 
@@ -7,11 +9,25 @@ with open("pop3.pcap", "rb") as f:
 	pcap = dpkt.pcap.Reader(f)
 
 	for ts, buf in pcap:
-		eth = dpkt.ethernet.Ethernet(buf)
-		ip = eth.data
-		tcp = ip.data
 
-		if  len(tcp.data) > 0:
-			print(tcp.data)
-			print("\n")
+		# Unpack the Ethernet frame (mac src/dst, ethertype)
+		eth = dpkt.ethernet.Ethernet(buf)
+
+		# Make sure the Ethernet data contains an IP packet
+		if not isinstance(eth.data, dpkt.ip.IP):
+			print ('Non IP Packet type not supported %s\n' % eth.data.__class__.__name__)
+			continue
+
+		# Now grab the data within the Ethernet frame (the IP packet)
+		ip = eth.data
+
+		# Check for TCP in the transport layer
+		if isinstance(ip.data, dpkt.tcp.TCP):
+
+			eth = dpkt.ethernet.Ethernet(buf)
+			tcp = ip.data
+
+			if  len(tcp.data) > 0:
+				print(tcp.data)
+				print("\n")
 	f.close()
